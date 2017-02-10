c17711b7e09b520d9b40af96e5c0f8f188ac1795
alex-blair <alex.n.blair@gmail.com>
2017-02-10 16:28:14 +1300

    refactor code

--- main.js
+++ main.js
@@ -1,55 +1,68 @@
+
 $(document).ready(function() {
+  var entry = $("#entry")
+  var history = $("#history")
   $(".btn").click(function(){
     //global variables
     var btnVal = $(this).val()
-    var entryStr = $("#entry").text()
-    var histStr = $("#history").text()
 
     if ($(this).hasClass("calculate")) {
       result = eval($("#history").text());
-      $("#entry").html(result);
-      $("#history").append("= " + result);
+      entry.html(result);
+      history.append("= " + result);
     }
     else if ($(this).hasClass("clearall")) {
       clearall();
     }
     else if ($(this).hasClass("clearlast")) {
-      if (histStr.includes("=")) {
+      if (history.text().includes("=")) {
         clearall()
       } else {
         clearlast()
       }
     }
-    else if (/^[0-9.]/.test(entryStr) === false) {
-      $("#entry").html(btnVal);
-      $("#history").append(btnVal);
+    else if ($(this).hasClass("operator")){
+      lastvalueHistory(operator)
+      switch(operator) {
+        case "+":
+        case "-":
+        case "*":
+        case "÷":
+        alert("Please don't put two operators in a row");
+        break;
+      }
     }
+    else if (/^[0-9.]/.test(entry.text()) === false) {
+      entry.html(btnVal);
+      history.append(btnVal);
+    }
+    // else if (isNaN(entry.text()) || entry.text() !== ".") {
+    //   entry.html(btnVal);
+    //   history.append(btnVal);
+    // }
     else if ($(this).hasClass("num")) {
-      // console.log($("#entry").text())
-      if ($("#history").text() === "0") {
-        $("#entry").html(btnVal);
-        $("#history").html(btnVal);
+      if (history.text() === "0") {
+        entry.html(btnVal);
+        history.html(btnVal);
       } else {
-        $("#entry").append(btnVal);
-        $("#history").append(btnVal);
+        entry.append(btnVal);
+        history.append(btnVal);
       }
     }
     else {
-      $("#entry").html(btnVal);
-      $("#history").append(btnVal);
+      entry.html(btnVal);
+      history.append(btnVal);
     }
   })
+  function clearall() {
+    entry.html("0");
+    history.html("0");
+  }
+  function clearlast() {
+    var arr = history.text().split(/(\D)/g);
+    arr.pop();
+    var joinArr = arr.join('');
+    entry.html('0');
+    history.html(joinArr);
+  }
 });
-
-function clearall() {
-  $("#entry").html("0");
-  $("#history").html("0");
-}
-
-function clearlast() {
-  var arr = $("#history").text().split(/(\D)/g);
-  arr.pop();
-  var joinArr = arr.join('');
-  $("#entry").html('0');
-  $("#history").html(joinArr);
-}
