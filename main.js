2d7388f5c61f17946f751389b7d25fb94d08238f
alex-blair <alex.n.blair@gmail.com>
2017-02-12 21:32:37 +1300

    refactor and fix bugs

--- main.js
+++ main.js
@@ -1,73 +1,62 @@
 
 $(document).ready(function() {
   var entry = $("#entry")
-  var history = $("#history")
+  var record = $("#record")
   $(".btn").click(function(){
-    //global variables
     var btnVal = $(this).val()
 
     if ($(this).hasClass("calculate")) {
-      result = eval($("#history").text());
+      result = eval(record.text());
       entry.html(result);
-      history.append("= " + result);
+      record.append("=" + result);
     }
-    else if ($(this).hasClass("clearall")) {
-      clearall();
-    }
-    else if ($(this).hasClass("clearlast")) {
-      if (history.text().includes("=")) {
-        clearall()
-      } else {
-        clearlast()
+    else if (record.text().includes("=")) {
+      if ($(this).hasClass("clearlast")) {
+        clearAll()
+      }
+      else if ($(this).hasClass("clearall")) {
+        clearAll()
+      }
+      else {
+        clearAll()
+        entry.html(btnVal);
+        record.html(btnVal);
       }
     }
-    // else if ($(this).hasClass("operator")){
-    //   lastvalueHistory(operator)
-    //   switch(operator) {
-    //     case "+":
-    //     case "-":
-    //     case "*":
-    //     case "÷":
-    //     alert("Please don't put two operators in a row");
-    //     break;
-    //   }
-    // }
+    else if ($(this).hasClass("clearall")) {
+      clearAll();
+    }
+    else if ($(this).hasClass("clearlast")) {
+      clearLast()
+    }
     else if (!/^[0-9.]/.test(entry.text())) {
       entry.html(btnVal);
-      history.append(btnVal);
+      record.append(btnVal);
     }
-    // else if (isNaN(entry.text()) || entry.text() !== ".") {
-    //   entry.html(btnVal);
-    //   history.append(btnVal);
-    // }
     else if ($(this).hasClass("num")) {
-      if (history.text() === "0") {
+      if (record.text() === "0") {
         entry.html(btnVal);
-        history.html(btnVal);
+        record.html(btnVal);
       } else {
         entry.append(btnVal);
-        history.append(btnVal);
+        record.append(btnVal);
       }
     }
     else {
       entry.html(btnVal);
-      history.append(btnVal);
+      record.append(btnVal);
     }
   })
-  function clearall() {
+  function clearAll() {
     entry.html("0");
-    history.html("0");
+    record.html("0");
   }
-  function clearlast() {
-    var arr = history.text().split(/(\D)/g);
-    arr.pop();
-    var joinArr = arr.join('');
-    entry.html('0');
-    history.html(joinArr);
+  function clearLast() {
+      var arr = record.text().split(/(\D)/g);
+      arr.pop();
+      var joinArr = arr.join('');
+      entry.html('0');
+      record.html(joinArr);
+    // }
   }
 });
-
-//
-// Bugs:
-// - once you press = and add to the number it adds to the number but you can't recalculate after that.
-// - when you try to clear one number
